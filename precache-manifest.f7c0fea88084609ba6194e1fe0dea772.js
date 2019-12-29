9efe6409bf22ec7b1524b977156cd6d1c555ffef
LakhaniSinan <sinan.lakhani09@gmail.com>
2019-12-30 01:49:41 +0500

    Updates

---
+++ precache-manifest.f7c0fea88084609ba6194e1fe0dea772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8723c7c2694901aaf59715c9449f1df",
+    "url": "/FunProjectReactJs/index.html"
+  },
+  {
+    "revision": "92604adb89a263aedc05",
+    "url": "/FunProjectReactJs/static/css/main.88649bfb.chunk.css"
+  },
+  {
+    "revision": "f9d27abe24e5e1dddf10",
+    "url": "/FunProjectReactJs/static/js/2.129521af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FunProjectReactJs/static/js/2.129521af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92604adb89a263aedc05",
+    "url": "/FunProjectReactJs/static/js/main.537bea1f.chunk.js"
+  },
+  {
+    "revision": "8f8d23e2149b1d11b0d6",
+    "url": "/FunProjectReactJs/static/js/runtime-main.b0537d21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FunProjectReactJs/static/media/logo.5d5d9eef.svg"
+  }
+]);
